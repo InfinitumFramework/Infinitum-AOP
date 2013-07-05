e94112df4b84544ed47a869e83fc989dd27883b4
Tyler Treat <ttreat31@gmail.com>
2013-07-04 22:46:41 -0500

    Fix NPE when <application> declaration is not included.

--- src/main/java/com/clarionmedia/infinitum/aop/context/impl/XmlInfinitumAopContext.java
+++ src/main/java/com/clarionmedia/infinitum/aop/context/impl/XmlInfinitumAopContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Clarion Media, LLC
+ * Copyright (C) 2013 Clarion Media, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,217 +48,214 @@ import java.lang.reflect.Method;
 import java.util.*;
 
 /**
- * <p>
- * Implementation of {@link InfinitumAopContext} which is initialized through
- * XML as a child of an {@link XmlApplicationContext} instance.
- * </p>
+ * <p> Implementation of {@link InfinitumAopContext} which is initialized through XML as a child of an {@link
+ * XmlApplicationContext} instance. </p>
  *
  * @author Tyler Treat
- * @version 1.0.4 03/13/13
+ * @version 1.1.0 07/04/13
  * @since 1.0
  */
 public class XmlInfinitumAopContext implements InfinitumAopContext {
 
-	private XmlApplicationContext mParentContext;
-	private List<InfinitumContext> mChildContexts;
-	private Map<String, Map<Integer, Object>> mMethodCache;
+    private XmlApplicationContext mParentContext;
+    private List<InfinitumContext> mChildContexts;
+    private Map<String, Map<Integer, Object>> mMethodCache;
     private ClassReflector mClassReflector;
 
-	/**
-	 * Creates a new {@code XmlInfinitumAopContext} instance as a child of the
-	 * given {@link XmlApplicationContext}.
-	 *
-	 * @param parentContext
-	 *            the parent of this context
-	 */
-	public XmlInfinitumAopContext(XmlApplicationContext parentContext) {
-		mParentContext = parentContext;
-		mChildContexts = new ArrayList<InfinitumContext>();
-		mMethodCache = new HashMap<String, Map<Integer, Object>>();
+    /**
+     * Creates a new {@code XmlInfinitumAopContext} instance as a child of the given {@link XmlApplicationContext}.
+     *
+     * @param parentContext the parent of this context
+     */
+    public XmlInfinitumAopContext(XmlApplicationContext parentContext) {
+        mParentContext = parentContext;
+        mChildContexts = new ArrayList<InfinitumContext>();
+        mMethodCache = new HashMap<String, Map<Integer, Object>>();
         mClassReflector = new JavaClassReflector();
-	}
+    }
 
-	@Override
-	public void postProcess(Context context) {
-		// Load aspect data from parent context
-		Set<XmlBean> xmlComponents = mParentContext.getXmlComponents();
-		Set<Class<?>> scannedAspects = getAndRemoveAspects(mParentContext.getScannedComponents());
+    @Override
+    public void postProcess(Context context) {
+        // Load aspect data from parent context
+        Set<XmlBean> xmlComponents = mParentContext.getXmlComponents();
+        Set<Class<?>> scannedAspects = getAndRemoveAspects(mParentContext.getScannedComponents());
 
-		// Transform aspects from XML or scanned form to generic definition
-		Set<AspectDefinition> aspects = transformAspects(xmlComponents, scannedAspects);
+        // Transform aspects from XML or scanned form to generic definition
+        Set<AspectDefinition> aspects = transformAspects(xmlComponents, scannedAspects);
 
-		// Add caching advice for cache abstraction
-		if (isCacheAbstractionEnabled())
-			addCachingAdvice(aspects);
+        // Add caching advice for cache abstraction
+        if (isCacheAbstractionEnabled())
+            addCachingAdvice(aspects);
 
         // Add events advice for event framework
         if (isEventsEnabled())
             addEventsAdvice(aspects);
 
-		// Process aspects
-		new ProxyingAspectWeaver(getBeanFactory(), new GenericPointcutBuilder(this), new DelegatingAdvisedProxyFactory()).weave(context,
-				aspects);
-	}
-
-	@Override
-	public List<AbstractBeanDefinition> getBeans(BeanDefinitionBuilder beanDefinitionBuilder) {
-		return new ArrayList<AbstractBeanDefinition>(0);
-	}
-
-	@Override
-	public boolean isCacheAbstractionEnabled() {
-		if (mParentContext.getAppConfig().containsKey("methodCaching"))
-			return Boolean.parseBoolean(mParentContext.getAppConfig().get("methodCaching"));
-		return false;
-	}
-
-	@Override
-	public Map<String, Map<Integer, Object>> getMethodCache() {
-		return mMethodCache;
-	}
-
-    @Override
-    public boolean isEventsEnabled() {
-        if (mParentContext.getAppConfig().containsKey("events"))
-            return Boolean.parseBoolean(mParentContext.getAppConfig().get("events"));
-        return false;
+        // Process aspects
+        new ProxyingAspectWeaver(getBeanFactory(), new GenericPointcutBuilder(this),
+                new DelegatingAdvisedProxyFactory()).weave(context,
+                aspects);
     }
 
     @Override
-	public boolean isDebug() {
-		return mParentContext.isDebug();
-	}
+    public List<AbstractBeanDefinition> getBeans(BeanDefinitionBuilder beanDefinitionBuilder) {
+        return new ArrayList<AbstractBeanDefinition>(0);
+    }
 
-	@Override
-	public Context getAndroidContext() {
-		return mParentContext.getAndroidContext();
-	}
+    @Override
+    public boolean isCacheAbstractionEnabled() {
+        Map<String, String> appConfig = mParentContext.getAppConfig();
+        return appConfig != null && appConfig.containsKey("methodCaching")
+                && Boolean.parseBoolean(mParentContext.getAppConfig().get("methodCaching"));
+    }
 
-	@Override
-	public BeanFactory getBeanFactory() {
-		return mParentContext.getBeanFactory();
-	}
+    @Override
+    public Map<String, Map<Integer, Object>> getMethodCache() {
+        return mMethodCache;
+    }
 
-	@Override
-	public Object getBean(String name) {
-		return mParentContext.getBean(name);
-	}
+    @Override
+    public boolean isEventsEnabled() {
+        Map<String, String> appConfig = mParentContext.getAppConfig();
+        return appConfig != null && appConfig.containsKey("events")
+                && Boolean.parseBoolean(mParentContext.getAppConfig().get("events"));
+    }
 
-	@Override
-	public <T> T getBean(String name, Class<T> clazz) {
-		return mParentContext.getBean(name, clazz);
-	}
+    @Override
+    public boolean isDebug() {
+        return mParentContext.isDebug();
+    }
 
-	@Override
-	public boolean isComponentScanEnabled() {
-		return mParentContext.isComponentScanEnabled();
-	}
+    @Override
+    public Context getAndroidContext() {
+        return mParentContext.getAndroidContext();
+    }
 
-	@Override
-	public List<InfinitumContext> getChildContexts() {
-		return mChildContexts;
-	}
+    @Override
+    public BeanFactory getBeanFactory() {
+        return mParentContext.getBeanFactory();
+    }
 
-	@Override
-	public void addChildContext(InfinitumContext context) {
-		mChildContexts.add(context);
-	}
+    @Override
+    public Object getBean(String name) {
+        return mParentContext.getBean(name);
+    }
 
-	@Override
-	public InfinitumContext getParentContext() {
-		return mParentContext;
-	}
+    @Override
+    public <T> T getBean(String name, Class<T> clazz) {
+        return mParentContext.getBean(name, clazz);
+    }
 
-	@Override
-	public <T extends InfinitumContext> T getChildContext(Class<T> contextType) {
-		return mParentContext.getChildContext(contextType);
-	}
+    @Override
+    public boolean isComponentScanEnabled() {
+        return mParentContext.isComponentScanEnabled();
+    }
 
-	@Override
-	public RestfulContext getRestContext() {
-		return mParentContext.getRestContext();
-	}
+    @Override
+    public List<InfinitumContext> getChildContexts() {
+        return mChildContexts;
+    }
 
-	@Override
-	public void publishEvent(AbstractEvent event) {
-		mParentContext.publishEvent(event);
-	}
+    @Override
+    public void addChildContext(InfinitumContext context) {
+        mChildContexts.add(context);
+    }
 
-	@Override
-	public void subscribeForEvents(EventSubscriber subscriber) {
-		mParentContext.subscribeForEvents(subscriber);
-	}
+    @Override
+    public InfinitumContext getParentContext() {
+        return mParentContext;
+    }
 
-	private Set<AspectDefinition> transformAspects(Set<XmlBean> xmlComponents, Set<Class<?>> scannedAspects) {
-		AspectTransformer transformer = new GenericAspectTransformer();
-		Set<AspectDefinition> aspects = new HashSet<AspectDefinition>();
+    @Override
+    public <T extends InfinitumContext> T getChildContext(Class<T> contextType) {
+        return mParentContext.getChildContext(contextType);
+    }
 
-		// Transform XML aspects
-		for (XmlBean component : xmlComponents) {
-			if (XmlAspect.class.isAssignableFrom(component.getClass())) {
-				aspects.add(transformer.transform((XmlAspect) component));
-			}
-		}
+    @Override
+    public RestfulContext getRestContext() {
+        return mParentContext.getRestContext();
+    }
 
-		// Transform scanned aspects
-		for (Class<?> aspect : scannedAspects)
-			aspects.add(transformer.transform(aspect));
+    @Override
+    public void publishEvent(AbstractEvent event) {
+        mParentContext.publishEvent(event);
+    }
 
-		return aspects;
-	}
+    @Override
+    public void subscribeForEvents(EventSubscriber subscriber) {
+        mParentContext.subscribeForEvents(subscriber);
+    }
 
-	private Set<Class<?>> getAndRemoveAspects(Collection<Class<?>> components) {
-		Set<Class<?>> aspects = new HashSet<Class<?>>();
-		Iterator<Class<?>> iter = components.iterator();
-		while (iter.hasNext()) {
-			Class<?> component = iter.next();
-			if (component.isAnnotationPresent(Aspect.class)) {
-				aspects.add(component);
-				iter.remove();
-			}
-		}
-		return aspects;
-	}
+    private Set<AspectDefinition> transformAspects(Set<XmlBean> xmlComponents, Set<Class<?>> scannedAspects) {
+        AspectTransformer transformer = new GenericAspectTransformer();
+        Set<AspectDefinition> aspects = new HashSet<AspectDefinition>();
 
-	private void addCachingAdvice(Set<AspectDefinition> aspects) {
-		AspectDefinition cachingAspect = new AspectDefinition();
-		cachingAspect.setName(StringUtil.toCamelCase(CacheAspect.class.getSimpleName()));
-		cachingAspect.setType(CacheAspect.class);
-		List<AdviceDefinition> adviceList = new ArrayList<AdviceDefinition>();
+        // Transform XML aspects
+        for (XmlBean component : xmlComponents) {
+            if (XmlAspect.class.isAssignableFrom(component.getClass())) {
+                aspects.add(transformer.transform((XmlAspect) component));
+            }
+        }
 
-		// Create Cache advice
-		AdviceDefinition cacheAdvice = new AdviceDefinition();
-		cacheAdvice.setType(AdviceLocation.Around);
-		cacheAdvice.setPointcutType("within");
-		cacheAdvice.setPointcutValue(new String[] { "*" });
-		Method method = mClassReflector.getMethod(CacheAspect.class, "cache", ProceedingJoinPoint.class);
-		cacheAdvice.setMethod(method);
-		cacheAdvice.setQualifier(new AdviceQualifier() {
-			@Override
-			public boolean qualifies(Class<?> clazz) {
-				return mClassReflector.containsMethodAnnotation(clazz, Cache.class);
-			}
-		});
-		adviceList.add(cacheAdvice);
+        // Transform scanned aspects
+        for (Class<?> aspect : scannedAspects)
+            aspects.add(transformer.transform(aspect));
 
-		// Create EvictCache advice
-		AdviceDefinition evictCacheAdvice = new AdviceDefinition();
-		evictCacheAdvice.setType(AdviceLocation.Before);
-		evictCacheAdvice.setPointcutType("within");
-		evictCacheAdvice.setPointcutValue(new String[] { "*" });
-		method = mClassReflector.getMethod(CacheAspect.class, "evictCache", JoinPoint.class);
-		evictCacheAdvice.setMethod(method);
-		evictCacheAdvice.setQualifier(new AdviceQualifier() {
-			@Override
-			public boolean qualifies(Class<?> clazz) {
-				return mClassReflector.containsMethodAnnotation(clazz, EvictCache.class);
-			}
-		});
-		adviceList.add(evictCacheAdvice);
+        return aspects;
+    }
 
-		cachingAspect.setAdvice(adviceList);
-		aspects.add(cachingAspect);
-	}
+    private Set<Class<?>> getAndRemoveAspects(Collection<Class<?>> components) {
+        Set<Class<?>> aspects = new HashSet<Class<?>>();
+        Iterator<Class<?>> iter = components.iterator();
+        while (iter.hasNext()) {
+            Class<?> component = iter.next();
+            if (component.isAnnotationPresent(Aspect.class)) {
+                aspects.add(component);
+                iter.remove();
+            }
+        }
+        return aspects;
+    }
+
+    private void addCachingAdvice(Set<AspectDefinition> aspects) {
+        AspectDefinition cachingAspect = new AspectDefinition();
+        cachingAspect.setName(StringUtil.toCamelCase(CacheAspect.class.getSimpleName()));
+        cachingAspect.setType(CacheAspect.class);
+        List<AdviceDefinition> adviceList = new ArrayList<AdviceDefinition>();
+
+        // Create Cache advice
+        AdviceDefinition cacheAdvice = new AdviceDefinition();
+        cacheAdvice.setType(AdviceLocation.Around);
+        cacheAdvice.setPointcutType("within");
+        cacheAdvice.setPointcutValue(new String[]{"*"});
+        Method method = mClassReflector.getMethod(CacheAspect.class, "cache", ProceedingJoinPoint.class);
+        cacheAdvice.setMethod(method);
+        cacheAdvice.setQualifier(new AdviceQualifier() {
+            @Override
+            public boolean qualifies(Class<?> clazz) {
+                return mClassReflector.containsMethodAnnotation(clazz, Cache.class);
+            }
+        });
+        adviceList.add(cacheAdvice);
+
+        // Create EvictCache advice
+        AdviceDefinition evictCacheAdvice = new AdviceDefinition();
+        evictCacheAdvice.setType(AdviceLocation.Before);
+        evictCacheAdvice.setPointcutType("within");
+        evictCacheAdvice.setPointcutValue(new String[]{"*"});
+        method = mClassReflector.getMethod(CacheAspect.class, "evictCache", JoinPoint.class);
+        evictCacheAdvice.setMethod(method);
+        evictCacheAdvice.setQualifier(new AdviceQualifier() {
+            @Override
+            public boolean qualifies(Class<?> clazz) {
+                return mClassReflector.containsMethodAnnotation(clazz, EvictCache.class);
+            }
+        });
+        adviceList.add(evictCacheAdvice);
+
+        cachingAspect.setAdvice(adviceList);
+        aspects.add(cachingAspect);
+    }
 
     private void addEventsAdvice(Set<AspectDefinition> aspects) {
         AspectDefinition eventsAspect = new AspectDefinition();
