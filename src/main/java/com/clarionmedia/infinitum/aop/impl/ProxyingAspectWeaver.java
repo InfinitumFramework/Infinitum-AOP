1c191f82a822402b8c6106412c366f9f70fe23ae
Tyler Treat <ttreat31@gmail.com>
2013-07-22 21:22:39 -0500

    Use bytecode instrumentation for proxies.

--- src/main/java/com/clarionmedia/infinitum/aop/impl/ProxyingAspectWeaver.java
+++ src/main/java/com/clarionmedia/infinitum/aop/impl/ProxyingAspectWeaver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Clarion Media, LLC
+ * Copyright (C) 2013 Clarion Media, LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,58 +16,52 @@
 
 package com.clarionmedia.infinitum.aop.impl;
 
-import java.util.Set;
-
 import android.content.Context;
-
-import com.clarionmedia.infinitum.aop.AdvisedProxyFactory;
-import com.clarionmedia.infinitum.aop.AspectDefinition;
-import com.clarionmedia.infinitum.aop.AspectWeaver;
-import com.clarionmedia.infinitum.aop.Pointcut;
-import com.clarionmedia.infinitum.aop.PointcutBuilder;
+import com.clarionmedia.infinitum.aop.*;
 import com.clarionmedia.infinitum.di.AbstractProxy;
 import com.clarionmedia.infinitum.di.BeanFactory;
 
+import java.util.Set;
+
 /**
- * <p>
- * Implementation of of {@link AspectWeaver} which uses proxies to advise
- * objects.
- * </p>
- * 
+ * <p> Implementation of of {@link AspectWeaver} which uses proxies to advise objects. </p>
+ *
  * @author Tyler Treat
- * @version 1.0 12/28/12
+ * @version 1.1.0.1 07/22/13
  * @since 1.0
  */
 public class ProxyingAspectWeaver implements AspectWeaver {
 
-	private PointcutBuilder mPointcutBuilder;
-	private BeanFactory mBeanFactory;
-	private AdvisedProxyFactory mProxyFactory;
+    private PointcutBuilder mPointcutBuilder;
+    private BeanFactory mBeanFactory;
+    private AdvisedProxyFactory mProxyFactory;
 
-	/**
-	 * Constructs a new {@code ProxyingAspectWeaver} instance.
-	 * 
-	 * @param beanFactory
-	 *            the {@link BeanFactory} the aspects are scoped to
-	 * @param pointcutBuilder
-	 *            {@link PointcutBuilder} to use
-	 * @param proxyFactory
-	 *            the {@link AdvisedProxyFactory} to use
-	 */
-	public ProxyingAspectWeaver(BeanFactory beanFactory, PointcutBuilder pointcutBuilder, AdvisedProxyFactory proxyFactory) {
-		mPointcutBuilder = pointcutBuilder;
-		mProxyFactory = proxyFactory;
-		mBeanFactory = beanFactory;
-	}
+    /**
+     * Constructs a new {@code ProxyingAspectWeaver} instance.
+     *
+     * @param beanFactory     the {@link BeanFactory} the aspects are scoped to
+     * @param pointcutBuilder {@link PointcutBuilder} to use
+     * @param proxyFactory    the {@link AdvisedProxyFactory} to use
+     */
+    public ProxyingAspectWeaver(BeanFactory beanFactory, PointcutBuilder pointcutBuilder,
+                                AdvisedProxyFactory proxyFactory) {
+        mPointcutBuilder = pointcutBuilder;
+        mProxyFactory = proxyFactory;
+        mBeanFactory = beanFactory;
+    }
 
-	@Override
-	public void weave(Context context, Set<AspectDefinition> aspects) {
-		for (Pointcut pointcut : mPointcutBuilder.build(aspects)) {
-			String beanName = pointcut.getBeanName();
-			Object bean = mBeanFactory.loadBean(beanName);
-			AbstractProxy proxy = mProxyFactory.createProxy(context, bean, pointcut);
-			mBeanFactory.getBeanDefinitions().get(beanName).setBeanProxy(proxy);
-		}
-	}
+    @Override
+    public void weave(Context context, Set<AspectDefinition> aspects) {
+        for (Pointcut pointcut : mPointcutBuilder.build(aspects)) {
+            String beanName = pointcut.getBeanName();
+            Object bean = mBeanFactory.loadBean(beanName);
+
+            // We are forcing bytecode instrumentation here as a workaround for cases where beans are being cast to
+            // concrete types.
+            AbstractProxy proxy = mProxyFactory.createProxy(context, bean, pointcut, true);
+
+            mBeanFactory.getBeanDefinitions().get(beanName).setBeanProxy(proxy);
+        }
+    }
 
 }
